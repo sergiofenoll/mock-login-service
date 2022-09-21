2ed34501a7874d974855531696825e8bc7d7ada7
Erika Pauwels <erika.pauwels@gmail.com>
2022-09-21 14:38:35 +0200

    Update SPARQL queries according to new authentication data model

--- login_service/sparql_queries.rb
+++ login_service/sparql_queries.rb
@@ -1,120 +1,85 @@
 require_relative '/usr/src/app/sinatra_template/utils.rb'
-require_relative '/config/config.rb'
 
 module LoginService
   module SparqlQueries
     include SinatraTemplate::Utils
-    include LoginConfig
+
     def remove_old_sessions(session)
       query = " DELETE WHERE {"
-      query += "   GRAPH <http://mu.semte.ch/graphs/sessions> {"
+      query += "   GRAPH <#{SESSIONS_GRAPH}> {"
       query += "     <#{session}> <#{MU_SESSION.account}> ?account ;"
       query += "                  <#{MU_CORE.uuid}> ?id ; "
       query += "                  <#{RDF::Vocab::DC.modified}> ?modified ; "
-      query += "                  <#{MU_EXT.sessionRole}> ?role ;"
-      query += "                  <#{MU_EXT.sessionGroup}> ?group ."
+      query += "                  <#{MU_EXT.sessionMembership}> ?membership ."
       query += "   }"
       query += " }"
       update(query)
     end
 
-    def insert_new_session_for_account(account, session_uri, session_id, group_uri, group_id, roles)
+    def insert_new_session_for_account(account, session_uri, session_id, membership_uri)
       now = DateTime.now
 
       query =  " PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>"
       query += " INSERT DATA {"
-      query += "   GRAPH <http://mu.semte.ch/graphs/sessions> {"
+      query += "   GRAPH <#{SESSIONS_GRAPH}> {"
       query += "     <#{session_uri}> <#{MU_SESSION.account}> <#{account}> ;"
       query += "                      <#{RDF::Vocab::DC.modified}> #{now.sparql_escape} ;"
-      query += "                      <#{MU_EXT.sessionGroup}> <#{group_uri}> ;"
+      query += "                      <#{MU_EXT.sessionMembership}> <#{membership_uri}> ;"
       query += "                      <#{MU_CORE.uuid}> #{session_id.sparql_escape} ."
-      roles.each do |role|
-        query += "   <#{session_uri}> <#{MU_EXT.sessionRole}> #{role.sparql_escape} ."
-      end
       query += "   }"
       query += " }"
       update(query)
     end
 
     def select_account_by_session(session)
-      query =  " SELECT ?session_uuid ?group_uuid ?account_uuid ?account (GROUP_CONCAT(?role; SEPARATOR = ',') as ?roles) WHERE {"
-      query += "   GRAPH <http://mu.semte.ch/graphs/sessions> {"
-      query += "     <#{session}> <#{MU_CORE.uuid}> ?session_uuid;"
-      query += "                  <#{MU_SESSION.account}> ?account ;"
-      query += "                  <#{MU_EXT.sessionRole}> ?role ;"
-      query += "                  <#{MU_EXT.sessionGroup}> ?group ."
+      query =  " SELECT ?session_id ?account_uri ?account_id ?membership_id WHERE {"
+      query += "   GRAPH <#{SESSIONS_GRAPH}> {"
+      query += "     <#{session}> <#{MU_CORE.uuid}> ?session_id;"
+      query += "                  <#{MU_SESSION.account}> ?account_uri ;"
+      query += "                  <#{MU_EXT.sessionMembership}> ?membership_uri ."
       query += "   }"
-      query += "   #{group_filter}"
-      query += "   GRAPH ?g {"
-      query += "     ?account a <#{RDF::Vocab::FOAF.OnlineAccount}> ;"
-      query += "              <#{MU_CORE.uuid}> ?account_uuid ."
+      query += "   GRAPH <#{MOCK_ACCOUNT_GRAPH}> {"
+      query += "     ?account_uri a <#{RDF::Vocab::FOAF.OnlineAccount}> ;"
+      query += "              <#{MU_CORE.uuid}> ?account_id ."
+      query += "     ?membership_uri a <#{ORG.Membership}> ;"
+      query += "              <#{MU_CORE.uuid}> ?membership_id ."
       query += "   }"
-      query += "   "
-      query += " } GROUP BY ?session_uuid ?group_uuid ?account_uuid ?account"
+      query += " } LIMIT 1"
       query(query)
     end
 
     def select_current_session(account)
       query =  " SELECT ?uri WHERE {"
-      query += "   GRAPH <http://mu.semte.ch/graphs/sessions> {"
+      query += "   GRAPH <#{SESSIONS_GRAPH}> {"
       query += "     ?uri <#{MU_SESSION.account}> <#{account}> ;"
       query += "        <#{MU_CORE.uuid}> ?id . "
       query += "   }"
-      query += " }"
+      query += " } LIMIT 1"
       query(query)
     end
 
     def delete_current_session(account)
       query = " DELETE WHERE {"
-      query += "   GRAPH <http://mu.semte.ch/graphs/sessions> {"
+      query += "   GRAPH <#{SESSIONS_GRAPH}> {"
       query += "     ?session <#{MU_SESSION.account}> <#{account}> ;"
       query += "              <#{MU_CORE.uuid}> ?id ; "
       query += "              <#{RDF::Vocab::DC.modified}> ?modified ; "
-      query += "              <#{MU_EXT.sessionRole}> ?role ;"
-      query += "              <#{MU_EXT.sessionGroup}> ?group ."
+      query += "              <#{MU_EXT.sessionMembership}> ?membership ."
       query += "   }"
       query += " }"
       update(query)
     end
 
-    def select_account(id)
-      query =  " SELECT ?uri WHERE {"
-      query += "   #{group_filter}"
-      query += "   GRAPH ?g {"
-      query += "     ?uri a <#{RDF::Vocab::FOAF.OnlineAccount}> ;"
-      query += "          <#{MU_CORE.uuid}> \"#{id}\" ."
-      query += "     ?person a <#{RDF::Vocab::FOAF.Person}> ;"
-      query += "             <#{RDF::Vocab::FOAF.account}> ?uri ;"
-      query += "             ^<#{RDF::Vocab::FOAF.member}> ?group ."
+    def select_account_and_membership(account_id)
+      query =  " SELECT ?account ?membership ?membership_id WHERE {"
+      query += "   GRAPH <#{MOCK_ACCOUNT_GRAPH}> {"
+      query += "     ?account a <#{RDF::Vocab::FOAF.OnlineAccount}> ;"
+      query += "          <#{MU_CORE.uuid}> #{account_id.sparql_escape} ."
+      query += "     ?person <#{RDF::Vocab::FOAF.account}> ?account ."
+      query += "     ?membership <#{ORG.member}> ?person ; "
+      query += "          <#{MU_CORE.uuid}> ?membership_id ."
       query += "   }"
-      query += "   BIND(IRI(CONCAT(\"http://mu.semte.ch/graphs/organizations/\", ?group_uuid)) as ?g)"
-      query += " }"
-      query(query)
-    end
-
-    def select_group(group_id)
-      restricted_filter = group_filter
-      restricted_filter = restricted_filter.gsub("?group_uuid", "\"#{group_id}\"")
-      query =  " SELECT ?group WHERE {"
-      query += "    #{restricted_filter}"
-      query += " }"
-      query(query)
-    end
-
-
-    def select_roles(account_id)
-      query =  " SELECT ?role WHERE {"
-      query += "   #{group_filter}"
-      query += "   GRAPH ?g {"
-      query += "     ?uri a <#{RDF::Vocab::FOAF.OnlineAccount}> ;"
-      query += "            <#{MU_CORE.uuid}> \"#{account_id}\" ."
-      query += "     ?person a <#{RDF::Vocab::FOAF.Person}> ;"
-      query += "             <#{RDF::Vocab::FOAF.account}> ?uri ;"
-      query += "             ^<#{RDF::Vocab::FOAF.member}> ?group ."
-      query += "     ?group <#{RDF::Vocab::FOAF.name}> ?role ."
-      query += "   }"
-      query += "   BIND(IRI(CONCAT(\"http://mu.semte.ch/graphs/organizations/\", ?group_uuid)) as ?g)"
-      query += " }"
+      query += " } LIMIT 1"
       query(query)
     end
   end
